8ef8522c07a17915990f832ff986773d81a33ee5
nicolasjp <78872277+nicolasjp@users.noreply.github.com>
2022-05-26 15:06:55 +0200

    debut liaison

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -11,5 +11,5 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
-  
-}
+
+}*/
